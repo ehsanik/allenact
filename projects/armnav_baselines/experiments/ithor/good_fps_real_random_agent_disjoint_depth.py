fixing the rgb normalization bug

projects/armnav_baselines/experiments/ithor/good_fps_real_random_agent_disjoint_depth.py
@@ -2,8 +2,8 @@ import ai2thor
 import gym
 
 from plugins.ithor_arm_plugin.ithor_arm_constants import ENV_ARGS
-from plugins.ithor_arm_plugin.ithor_arm_sensors import RelativeAgentArmToObjectSensor, RelativeObjectToGoalSensor, PickedUpObjSensor, DepthSensorThor, BlindSensorThor
-from plugins.ithor_arm_plugin.ithor_arm_task_samplers import GoodFPSRandomAgentWDoneActionTaskSampler, WDoneActionTaskSampler
+from plugins.ithor_arm_plugin.ithor_arm_sensors import RelativeAgentArmToObjectSensor, RelativeObjectToGoalSensor, PickedUpObjSensor, DepthSensorThor
+from plugins.ithor_arm_plugin.ithor_arm_task_samplers import RandomAgentWDoneActionTaskSampler, WDoneActionTaskSampler, GoodFPSRandomAgentWDoneActionTaskSampler
 from plugins.ithor_arm_plugin.ithor_arm_tasks import PickUpDropOffTask, WDoneActionTask
 from plugins.ithor_plugin.ithor_sensors import RGBSensorThor
 
@@ -19,20 +19,21 @@ from projects.armnav_baselines.experiments.armnav_mixin_simplegru import (
 import torch.nn as nn
 
 from projects.armnav_baselines.models.arm_nav_models import ArmNavBaselineActorCritic
+from projects.armnav_baselines.models.disjoint_arm_nav_models import DisjointArmNavBaselineActorCritic
 
 
-class GoodFPSRealRGBRandomAgentLocWDoneArmNav(
+class GoodFPSRealDisjointDepthRandomAgentLocArmNav(
     ArmNaviThorBaseConfig, ArmNavMixInPPOConfig, ArmNavMixInSimpleGRUConfig,
 ):
     """An Object Navigation experiment configuration in iThor with RGB
     input."""
 
     SENSORS = [
-        RGBSensorThor(
+        DepthSensorThor(
             height=ArmNaviThorBaseConfig.SCREEN_SIZE,
             width=ArmNaviThorBaseConfig.SCREEN_SIZE,
-            use_resnet_normalization=True,
-            uuid="rgb_lowres",
+            use_normalization=True,
+            uuid="depth_lowres",
         ),
         # GoalObjectTypeThorSensor(object_types=ArmNaviThorBaseConfig.OBJECT_TYPES,),
         RelativeAgentArmToObjectSensor(),
@@ -41,17 +42,24 @@ class GoodFPSRealRGBRandomAgentLocWDoneArmNav(
     ]
 
     MAX_STEPS = 200
-    TASK_SAMPLER = GoodFPSRandomAgentWDoneActionTaskSampler #
-    # TASK_SAMPLER = WDoneActionTaskSampler
+    TASK_SAMPLER = GoodFPSRandomAgentWDoneActionTaskSampler
+    # TASK_SAMPLER  =WDoneActionTaskSampler
 
 
     def __init__(self):
         super().__init__()
 
         assert self.CAMERA_WIDTH == 224 and self.CAMERA_HEIGHT == 224 and self.VISIBILITY_DISTANCE == 1 and self.STEP_SIZE == 0.25
-        self.ENV_ARGS = {**ENV_ARGS,  'renderDepthImage':False}
+        self.ENV_ARGS = {**ENV_ARGS,  'renderDepthImage':True}
 
 
+    @classmethod
+    def create_model(cls, **kwargs) -> nn.Module:
+        return DisjointArmNavBaselineActorCritic(
+            action_space=gym.spaces.Discrete(len(cls.TASK_SAMPLER._TASK_TYPE.class_action_names())),
+            observation_space=kwargs["sensor_preprocessor_graph"].observation_spaces,
+            hidden_size=512,
+        )
 
     @classmethod
     def tag(cls):

projects/armnav_baselines/experiments/ithor/test_on_unseen_obj_seen_scenes_real_random_agent_depth.py
@@ -18,27 +18,16 @@ from projects.armnav_baselines.experiments.armnav_mixin_simplegru import (
 )
 import torch.nn as nn
 
+from projects.armnav_baselines.experiments.ithor.real_random_agent_depth import RealDepthRandomAgentLocArmNav
 from projects.armnav_baselines.models.arm_nav_models import ArmNavBaselineActorCritic
 
 
-class RealDepthRandomAgentLocArmNav(
-    ArmNaviThorBaseConfig, ArmNavMixInPPOConfig, ArmNavMixInSimpleGRUConfig,
+class TestOnUObjSSceneRealDepthRandomAgentLocArmNav(
+    RealDepthRandomAgentLocArmNav
 ):
     """An Object Navigation experiment configuration in iThor with RGB
     input."""
 
-    SENSORS = [
-        DepthSensorThor(
-            height=ArmNaviThorBaseConfig.SCREEN_SIZE,
-            width=ArmNaviThorBaseConfig.SCREEN_SIZE,
-            use_normalization=True,
-            uuid="depth_lowres",
-        ),
-        # GoalObjectTypeThorSensor(object_types=ArmNaviThorBaseConfig.OBJECT_TYPES,),
-        RelativeAgentArmToObjectSensor(),
-        RelativeObjectToGoalSensor(),
-        PickedUpObjSensor(),
-    ]
 
     MAX_STEPS = 200
     TASK_SAMPLER = RandomAgentWDoneActionTaskSampler
@@ -47,14 +36,3 @@ class RealDepthRandomAgentLocArmNav(
     # TASK_SAMPLER  =WDoneActionTaskSampler
 
 
-    def __init__(self):
-        super().__init__()
-
-        assert self.CAMERA_WIDTH == 224 and self.CAMERA_HEIGHT == 224 and self.VISIBILITY_DISTANCE == 1 and self.STEP_SIZE == 0.25
-        self.ENV_ARGS = {**ENV_ARGS,  'renderDepthImage':True}
-
-
-
-    @classmethod
-    def tag(cls):
-        return cls.__name__